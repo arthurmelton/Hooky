3137ff0a6429f7949292859457e915015559eb58
Arthur Melton <me@arthurmelton.com>
2023-01-03 22:23:49 -0500

    spelt firefox wrong

--- data/src/all_data/discord_firefox.rs
+++ data/src/all_data/discord_firefox.rs
@@ -2,7 +2,7 @@ use crate::Sends;
 
 impl Sends {
     #[cfg(all(feature = "discord-firefox", target_os = "windows"))]
-    pub fn discord_firfox(&mut self) -> Option<()> {
+    pub fn discord_firefox(&mut self) -> Option<()> {
         None
     }
 
